bc40cda41cb203aa60e0700e7f789a7b2b9b026a
jspengeman <jonathan.spengeman@gmail.com>
2025-03-22 14:30:05 -0700

    Basic gallery.

---
+++ src/utils/gallery.js
@@ -0,0 +1,12 @@
+export const getAllImages = async () => {
+    return [
+        "https://images.pexels.com/photos/7179373/pexels-photo-7179373.jpeg",
+        "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg",
+        "https://images.pexels.com/photos/4584855/pexels-photo-4584855.jpeg",
+        "https://images.pexels.com/photos/14721103/pexels-photo-14721103.jpeg",
+        "https://images.pexels.com/photos/11399825/pexels-photo-11399825.jpeg",
+        "https://images.pexels.com/photos/14660912/pexels-photo-14660912.jpeg",
+        "https://images.pexels.com/photos/1438649/pexels-photo-1438649.jpeg",
+        "https://images.pexels.com/photos/16918021/pexels-photo-16918021/free-photo-of-cat-portrait-in-black-and-white.jpeg",
+    ]
+}
