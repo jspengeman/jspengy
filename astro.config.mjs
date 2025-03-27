a0d98ed1aecdeefc8e2e922e04203a01ab30b692
jspengeman <jonathan.spengeman@gmail.com>
2025-03-26 21:55:42 -0700

    Unpic configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+
+import { imageService } from "@unpic/astro/service";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://spengy.com",
-  prefetch: true,
-  
-  vite: {
-    plugins: [tailwindcss()]
-  },
+    site: "https://spengy.com",
+    prefetch: true,
 
-  adapter: netlify(),
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-  image: {
-    domains: ['ik.imagekit.io'],
-  },
+    adapter: netlify(),
 
-  integrations: [sitemap()],
+    image: {
+        domains: ["ik.imagekit.io"],
+        service: imageService({
+            placeholder: "blurhash",
+            layout: "fullWidth",
+        }),
+    },
+
+    integrations: [sitemap()],
 });
