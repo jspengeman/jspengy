11bd4b2352ccb10c4fdf565c67fd773ecf29c3da
jspengeman <jonathan.spengeman@gmail.com>
2025-03-26 18:45:35 -0700

    Fixing up transistions.

--- src/utils/gallery.js
+++ src/utils/gallery.js
@@ -1,32 +1,32 @@
-const IMAGE_KIT_URL = "https://ik.imagekit.io/pmbw7zrkob"
+const IMAGE_KIT_URL = "https://ik.imagekit.io/pmbw7zrkob";
 
 export const getImageUrl = (filePath) => {
-    return `${IMAGE_KIT_URL}${filePath}`
-}
+    return `${IMAGE_KIT_URL}${filePath}`;
+};
 
 const newImageModel = (currResult, prevResult, nextResult) => ({
     src: getImageUrl(currResult.filePath),
     fileName: currResult.name,
     prevFileName: prevResult?.name,
-    nextFileName: nextResult?.name
-})
+    nextFileName: nextResult?.name,
+});
 
 export const getAllImages = async () => {
     const token = import.meta.env.IMAGE_KIT_KEY;
 
-    const headers = { 
-        "Accept": "application/json",
-        "Authorization": `Basic ${btoa(token)}`
+    const headers = {
+        Accept: "application/json",
+        Authorization: `Basic ${btoa(token)}`,
     };
     const query = "fileType=image&sort=ASC_CREATED";
-    const response = await fetch(`https://api.imagekit.io/v1/files?${query}`, { 
-        method: 'GET',
-        headers: headers 
+    const response = await fetch(`https://api.imagekit.io/v1/files?${query}`, {
+        method: "GET",
+        headers: headers,
     });
 
     if (!response.ok) {
-        console.log("ImageKit response:", response)
-        throw new Error("Failed to search ImageKit. Failing build.")
+        console.log("ImageKit response:", response);
+        throw new Error("Failed to search ImageKit. Failing build.");
     }
 
     const json = await response.json();
@@ -36,11 +36,12 @@ export const getAllImages = async () => {
         const prev = i - 1;
         const next = i + 1;
         const currResult = newImageModel(
-            json[i], 
-            prev in json ? json[prev] : null, 
-            next in json ? json[next] : null)
-        results.push(currResult)
+            json[i],
+            prev in json ? json[prev] : null,
+            next in json ? json[next] : null
+        );
+        results.push(currResult);
     }
 
     return results;
-}
+};
