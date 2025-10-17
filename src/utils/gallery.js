93db638adfd1586abdeb040111d6a1ec35a3abd5
Jon Spengeman <jon@spengy.com>
2025-10-16 20:15:07 -0700

    Incremental build (I)

--- src/utils/gallery.js
+++ src/utils/gallery.js
@@ -108,7 +108,7 @@ export const getAllImages = async () => {
     const json = await searchForImages();
 
     const images = [];
-    for (let i = 0; i < json.length; i++) {
+    for (let i = 0; i < 50; i++) {
         const { url, name, filePath, updatedAt } = json[i];
         const style = await getImageStyle(url, filePath, updatedAt);
         const { height, width } = await inferRemoteSize(url);
